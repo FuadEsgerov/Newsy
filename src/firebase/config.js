a715b314e25a87dfc7f36f274e0372eb84954904
FuadEsgerov <fuadra@code.edu.az>
2020-08-07 17:16:44 +0400

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmmko_d6033d-pl2fCMDuo_3NeAsPmUac",
+    authDomain: "news-1380e.firebaseapp.com",
+    databaseURL: "https://news-1380e.firebaseio.com",
+    projectId: "news-1380e",
+    storageBucket: "news-1380e.appspot.com",
+    messagingSenderId: "1002539325312",
+    appId: "1:1002539325312:web:689650ea906c447afa5365",
+    measurementId: "G-KNWBF44Y93"
+  };
+
+  exports default firebaseConfig ;
