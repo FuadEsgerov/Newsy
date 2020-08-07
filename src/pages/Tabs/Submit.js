7726d23f38560c2a95cdd831c17af9ca80b9c54c
FuadEsgerov <fuadra@code.edu.az>
2020-08-07 22:55:51 +0400

    Submit Links with Firebase

--- src/pages/Tabs/Submit.js
+++ src/pages/Tabs/Submit.js
@@ -1,17 +1,95 @@
 import React from "react";
 import {
-  IonPage, 
-  IonContent
+  IonContent,
+  IonPage,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonRow,
+  IonCol,
+  IonButton,
 } from "@ionic/react";
+import useForm from "../../hooks/useForm";
+import validateCreateLink from "../../validators/validateCreateLink";
+import firebase from "../../firebase";
+import UserContext from "../../contexts/UserContext";
 import SmallHeader from "../../components/Header/SmallHeader";
 import LargeHeader from "../../components/Header/LargeHeader";
 
-const Submit = () => {
+const INITIAL_STATE = {
+  description: "",
+  url: "",
+};
+
+const Submit = (props) => {
+  const { user } = React.useContext(UserContext);
+  const { handleSubmit, handleChange, values } = useForm(
+    INITIAL_STATE,
+    validateCreateLink,
+    handleCreateLink
+  );
+
+  function handleCreateLink() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName,
+        },
+        voteCount: 1,
+        votes: [],
+        comments: [],
+        created: Date.now(),
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
+  }
+
   return (
     <IonPage>
-			<SmallHeader title="Submit" />
-      <IonContent fullscreen>
+      <SmallHeader title="Submit" />
+      <IonContent>
         <LargeHeader title="Submit" />
+        <IonItem lines="full">
+          <IonLabel position="floating">Description</IonLabel>
+          <IonInput
+            name="description"
+            value={values.description}
+            type="text"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
+
+        <IonItem lines="full">
+          <IonLabel position="floating">URL</IonLabel>
+          <IonInput
+            name="url"
+            type="url"
+            value={values.url}
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
+
+        <IonRow>
+          <IonCol>
+            <IonButton
+              type="submit"
+              color="primary"
+              expand="block"
+              onClick={handleSubmit}
+            >
+              Submit
+            </IonButton>
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
---
+++ src/validators/validateCreateLink.js
@@ -0,0 +1,18 @@
+export default function validateCreateLink(values) {
+    let errors = {};
+  
+    // Description Errors
+    if (!values.description) {
+      errors.description = "A description is required.";
+    } else if (values.description.length < 10) {
+      errors.description = "The description must be at least 10 characters.";
+    }
+    // URL Errors
+    if (!values.url) {
+      errors.url = "A URL required.";
+    } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+      errors.url = "The URL must be valid.";
+    }
+  
+    return errors;
+  }
