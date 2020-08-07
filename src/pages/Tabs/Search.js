59544a94c769f7879b987cae4e6a52e43cb2964a
FuadEsgerov <fuadra@code.edu.az>
2020-08-07 23:20:03 +0400

    Search Page

--- src/pages/Tabs/Search.js
+++ src/pages/Tabs/Search.js
@@ -1,17 +1,77 @@
 import React from "react";
-import {
-  IonPage, 
-  IonContent
-} from "@ionic/react";
-import SmallHeader from "../../components/Header/SmallHeader";
+import { IonContent, IonPage, IonSearchbar } from "@ionic/react";
+import firebase from "../../firebase";
+import LinkItem from "../../components/Link/LinkItem";
 import LargeHeader from "../../components/Header/LargeHeader";
+import SmallHeader from "../../components/Header/SmallHeader";
 
 const Search = () => {
+  const [filteredLinks, setFilteredLinks] = React.useState([]);
+  const [links, setLinks] = React.useState([]);
+  const [filter, setFilter] = React.useState("");
+
+  React.useEffect(() => {
+    getInitialLinks();
+    // eslint-disable-next-line
+  }, []);
+
+  React.useEffect(() => {
+    handleSearch();
+    // eslint-disable-next-line
+  }, [filter]);
+
+  function getInitialLinks() {
+    firebase.db
+      .collection("links")
+      .get()
+      .then((snapshot) => {
+        const links = snapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+        setLinks(links);
+      });
+  }
+
+  function handleChange(evt) {
+    if (evt.key === "Enter") {
+      setFilter(evt.target.value);
+    }
+  }
+
+  function handleSearch() {
+    const query = filter.toLowerCase();
+    const matchedLinks = links.filter((link) => {
+      return (
+        link.description.toLowerCase().includes(query) ||
+        link.url.toLowerCase().includes(query) ||
+        link.postedBy.name.toLowerCase().includes(query)
+      );
+    });
+    setFilteredLinks(matchedLinks);
+  }
+
   return (
     <IonPage>
-			<SmallHeader title="Search" />
-      <IonContent fullscreen>
+      <SmallHeader title="Search" />
+      <IonContent>
         <LargeHeader title="Search" />
+        <IonSearchbar
+          placeholder="Search"
+          spellcheck="false"
+          type="url"
+          value={filter}
+          onKeyPress={handleChange}
+          animated
+        />
+        {filteredLinks.map((filteredLink, index) => (
+          <LinkItem
+            key={filteredLink.id}
+            showCount={false}
+            link={filteredLink}
+            index={index}
+            url={`/link/${filteredLink.id}`}
+          />
+        ))}
       </IonContent>
     </IonPage>
   );
