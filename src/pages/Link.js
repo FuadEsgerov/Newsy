b68ae9b067cebb3dd0e813e248b1ad9ce2006550
FuadEsgerov <fuadra@code.edu.az>
2020-08-07 23:18:16 +0400

    Upvote And Comment

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -15,12 +15,15 @@ import { Plugins } from "@capacitor/core";
 import UserContext from "../contexts/UserContext";
 import NavHeader from "../components/Header/NavHeader";
 import LinkItem from "../components/Link/LinkItem";
+import LinkComment from "../components/Link/LinkComment";
+import CommentModal from "../components/Link/CommentModal";
 
 const { Browser } = Plugins;
 
 const Link = (props) => {
   const { user } = React.useContext(UserContext);
   const [link, setLink] = React.useState(null);
+  const [showModal, setShowModal] = React.useState(false);
   const linkId = props.match.params.linkId;
   const linkRef = firebase.db.collection("links").doc(linkId);
 
@@ -33,6 +36,63 @@ const Link = (props) => {
     linkRef.get().then((doc) => {
       setLink({ ...doc.data(), id: doc.id });
     });
+  }
+
+  function handleOpenModal() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      setShowModal(true);
+    }
+  }
+
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+
+  function handleAddComment(commentText) {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      linkRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousComments = doc.data().comments;
+          const newComment = {
+            postedBy: { id: user.uid, name: user.displayName },
+            created: Date.now(),
+            text: commentText,
+          };
+          const updatedComments = [...previousComments, newComment];
+          linkRef.update({ comments: updatedComments });
+          setLink((prevState) => ({
+            ...prevState,
+            comments: updatedComments,
+          }));
+        }
+      });
+      setShowModal(false);
+    }
+  }
+
+  function handleAddVote() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      linkRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          const voteCount = updatedVotes.length;
+          linkRef.update({ votes: updatedVotes, voteCount });
+          setLink((prevState) => ({
+            ...prevState,
+            votes: updatedVotes,
+            voteCount: voteCount,
+          }));
+        }
+      });
+    }
   }
 
   function handleDeleteLink() {
@@ -66,20 +126,41 @@ const Link = (props) => {
         action={handleDeleteLink}
       />
       <IonContent>
+        <CommentModal
+          isOpen={showModal}
+          title="New Comment"
+          sendAction={handleAddComment}
+          closeAction={handleCloseModal}
+        />
         {link && (
           <>
             <IonGrid>
               <IonRow>
                 <IonCol class="ion-text-center">
                   <LinkItem link={link} browser={openBrowser} />
+                  <IonButton onClick={() => handleAddVote()} size="small">
+                    Upvote
+                  </IonButton>
+                  <IonButton onClick={() => handleOpenModal()} size="small">
+                    Comment
+                  </IonButton>
                 </IonCol>
               </IonRow>
             </IonGrid>
+            {link.comments.map((comment, index) => (
+              <LinkComment
+                key={index}
+                comment={comment}
+                link={link}
+                setLink={setLink}
+              />
+            ))}
           </>
         )}
       </IonContent>
     </IonPage>
   );
 };
+
 
 export default Link;
